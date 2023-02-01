a07a9b5cff1ba09924d9277b7bdb6356fbf0b8bd
Kevin QUACH <kevinquach93270@gmail.com>
2023-02-01 23:26:25 +0100

    TP2 C++

--- src/translator.cpp
+++ src/translator.cpp
@@ -4,60 +4,124 @@
 #include <map>
 #include <string_view>
 #include <sstream>
+#include <vector>
+#include <fstream>
 using namespace std;
 
-std::set<std::string> make_exit_commands(){
-    std::set<std::string> set {"q", "quit", "e", "exit"};
+using Translate = map<std::string, std::string>;
+using History = std::vector<string>;
+using Language = pair<string, string>;
+
+std::set<std::string> make_exit_commands()
+{
+    std::set<std::string> set{"q", "quit", "e", "exit"};
     return set;
 }
 
-int main()
+void execute_command(string command, Translate &translate, History &history, std::istream &stream)
 {
+    if (command == "add")
+    {
+        string str1;
+        string str2;
+        stream >> str1;
+        stream >> str2;
+        translate.emplace(str1, str2);
+        std::cout << str1 + "=>" + str2 << std::endl;
+        history.push_back("add " + str1 + " " + str2);
+        str1 = "";
+        str2 = "";
+    }
+    else if (command == "translate")
+    {
+        std::string line;
+        std::getline(stream, line);
+
+        std::stringstream words;
+        std::string word;
+        words << line;
+        while (!words.eof())
+        {
+            words >> word;
+            auto it = translate.find(word);
+            if (it != translate.cend())
+            {
+                cout << it->second << endl;
+            }
+            else
+            {
+                cout << "???" << endl;
+            }
+        }
+    }
+    else if (command == "print")
+    {
+        for (auto it : translate)
+        {
+            cout << it.first + "=>" + it.second << endl;
+        }
+    }
+    else if (command == "save")
+    {
+        std::string filename;
+        stream >> filename;
+        std::ofstream f(filename, std::ios::out);
+        for (const auto &str : history)
+        {
+            f << str << "\n";
+        }
+    }
+    else if (command == "load")
+    {
+        std::string filename;
+        stream >> filename;
+        auto f = std::ifstream{filename};
+        while (!f.eof())
+        {
+            std::string add;
+            f >> add;
+            execute_command(add, translate, history, f);
+        }
+    }
+    else if (command == "clear")
+    {
+        translate.clear();
+        history.clear();
+        cout << "Clearing every translations" << endl;
+    }
+    else if (command == "remove")
+    {
+        string str;
+        stream >> str;
+        for (auto it = translate.begin(); it != translate.end();)
+        {
+            if (it->first == str || it->second == str)
+            {
+                std::cout << it->first << " x " << it->second << std::endl;
+                it = translate.erase(it);
+            }
+            else
+            {
+                it++;
+            }
+        }
+    }
+}
+
+int main(int argc, char **argv)
+{
+    Language language = argc == 3 ? Language{argv[1], argv[2]} : Language{"fr", "en"};
     std::set<std::string> exits_command = make_exit_commands();
-    std::map<std::string, std::string> translate;
-    std::string string;
+    Translate translate;
+    History history;
+    std::string command;
     std::string str1 = "";
     std::string str2 = "";
     do
     {
-        std::cin >> string;
-        if (string.find("add") != std::string::npos){
-            std::cin >> str1;
-            std::cin >> str2;
-            translate.emplace(str1, str2);
-            std::cout << str1 + "=>" + str2 << std::endl;
-            str1 = "";
-            str2 = "";
-        }
-        if (string.find("translate") != std::string::npos){
-            std::string line;
-            std::getline(std::cin, line);
-
-            std::stringstream words;
-            std::string word;
-            words << line;
-            words >> word;
-            while (words.eof()){
-                auto it = translate.find(word);
-                if (it != translate.cend()){
-                    cout << it->second << endl;
-                }
-                else {
-                    cout << "???" << endl;
-                }
-            }
-
-
-            // std::cin >> str1;
-            // auto it = translate.find(str1);
-            // if (it != translate.cend()){
-            //     cout << it->second << endl;
-            // }
-            // else {
-            //     cout << "???" << endl;
-            // }
-        }
-    } while (exits_command.count(string) == 0);
+        std::cin >> command;
+        execute_command(command, translate, history, std::cin);
+    } while (exits_command.count(command) == 0);
 
     return 0;
 }
