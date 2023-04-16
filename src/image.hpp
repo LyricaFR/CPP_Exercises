5f2ac5f4ef805854967859fffd7587c59156fbca
Kevin QUACH <kevinquach93270@gmail.com>
2023-04-16 18:13:13 +0200

    Saved Changes

--- src/image.hpp
+++ src/image.hpp
@@ -1,60 +1,60 @@
 #pragma once
 
+#include "../lib/lib.hpp"
 #include "pixels.hpp"
 
-#include "../lib/lib.hpp"
-
-#include <stdint.h>
+#include <array>
 #include <cstddef>
 #include <functional>
-#include <string>
-#include <array>
 #include <iostream>
+#include <stdint.h>
+#include <string>
 
-template<typename P, size_t W, size_t H>
+template <typename P, size_t W, size_t H>
 class Image
 {
-    public:
-
+public:
     Image() = default;
 
-    Image(const P& pix){
-        for (size_t i = 0; i < H ; i++){
-            for (size_t j = 0; j < W; j++){
+    Image(const P& pix)
+    {
+        for (size_t i = 0; i < H; i++)
+        {
+            for (size_t j = 0; j < W; j++)
+            {
                 img[i][j] = pix;
             }
         }
     }
 
-    P & operator()(const size_t i, const size_t j){
-        return img[j][i];
-    }
+    P& operator()(const size_t i, const size_t j) { return img[j][i]; }
 
-    const P & operator()(const size_t i, const size_t j) const{
-        return img[j][i];
-    }
+    const P& operator()(const size_t i, const size_t j) const { return img[j][i]; }
 
-    friend Image<P,W,H> operator+(const Image<P, W, H>& img1, const Image<P, W, H>& img2){
+    friend Image<P, W, H> operator+(const Image<P, W, H>& img1, const Image<P, W, H>& img2)
+    {
         auto result = Image<P, W, H>();
-        for (size_t i = 0; i < H ; i++){
-            for (size_t j = 0; j < W; j++){
-                result(i,j) = img1(i, j) + img2(i, j);
+        for (size_t i = 0; i < H; i++)
+        {
+            for (size_t j = 0; j < W; j++)
+            {
+                result(i, j) = img1(i, j) + img2(i, j);
             }
         }
         return result;
     }
 
-    Image<P, W, H>(const std::function<P(const size_t i, const size_t j)> & functor)
+    Image<P, W, H>(const std::function<P(const size_t i, const size_t j)>& functor)
     {
-        for (size_t i = 0; i < H ; i++){
-            for (size_t j = 0; j < W; j++){
+        for (size_t i = 0; i < H; i++)
+        {
+            for (size_t j = 0; j < W; j++)
+            {
                 img[i][j] = functor(j, i);
             }
         }
     }
 
-    private:
+private:
     std::array<std::array<P, W>, H> img;
 };
-
-
--- src/pixels.hpp
+++ src/pixels.hpp
@@ -23,3 +23,44 @@ struct Luma
 {
     uint8_t gray;
 };
+
+template <typename A, typename B>
+A from(const B& stuff)
+{}
+
+template <>
+RGB from(const RGBA& x)
+{
+    return { x.r, x.g, x.b };
+}
+
+template <>
+RGBA from(const RGB& x)
+{
+    return { x.r, x.g, x.b, 255 };
+}
+
+template <>
+RGB from(const Luma& x)
+{
+    const auto [r, g, b] = lib::grayscale_to_rgb(x.gray);
+    return { r, g, b };
+}
+
+template <>
+Luma from(const RGB& x)
+{
+    return { lib::rgb_to_grayscale(x.r, x.g, x.b) };
+}
+
+template <>
+Luma from(const RGBA& x)
+{
+    return { from<Luma, RGB>(from<RGB, RGBA>(x)) };
+}
+
+template <>
+RGBA from(const Luma& x)
+{
+    return { from<RGBA, RGB>(from<RGB, Luma>(x)) };
+}
